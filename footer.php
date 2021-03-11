917ea4d7c94e223e65487af568e8fd4c49c05adf
Vincent Chauvaux <vincent.chauvaux@gmail.com>
2021-03-11 16:36:06 +0100

    mise à jour du site

---
+++ footer.php
@@ -0,0 +1,25 @@
+
+            <footer class="bg-black shadow absolute inset-x-0 bottom-0">
+                <div class="container mx-auto flex justify-center">
+    
+                    <p class="h-30 flex flex-wrap content-center text-white">© 2021 Vincent Chauvaux</p>
+                    
+                    <div class="space-x-10 ml-auto flex py-2">
+                        
+    
+                        <img src="icon/facebook.png" alt="" class="h-8">
+                        <img src="icon/insta.png" alt="" class="h-8">
+                    </div>
+
+                </div>
+            </footer>
+         
+    </div>
+
+
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/styles.css">
+    <title>À propos</title>
+</head>
+<body>
+    <div class="h-screen relative">
+        
+            <nav class="bg-black shadow-lg">
+                <div class="container mx-auto flex py-2">
+                    <a href="index.html" class="mr-auto w-10 transform hover:scale-110 motion-reduce:transform-none"><img src="svg/logo-blanc.svg"alt="" class=""></a>
+                    <ul class="flex flex-wrap content-center text-right text-white space-x-10 ">
+                        <li> <a href="index.html">Accueil</a></li>
+                        <li> <a href="apropo.html">A propos</a></li>
+                        <li>Portfolio</li>
+                        <li>Contact</li>
+                    </ul>
+                </div>
+            </nav>
